14880d1de9ff0a12740f3fb369f55eb40340534f
Jchan <gkswocks123@gmail.com>
2023-02-09 17:57:18 +0900

    ✨ item.getData 오퍼레이션에 맞는 메서드 구현 - Item 추상 클래스 생성

--- example-test-jc/src/customer/Customer.java
+++ example-test-jc/src/customer/Customer.java
@@ -1,6 +1,13 @@
+import item.Item;
+
 public class Customer {
-    // A를 구매하라
     Customer(){}
 
-    public void BuyItem() {}
+    public void BuyItem(Item item) {
+        String itemData = item.getData(); // [?] 적절하게 캡슐화되었나?
+    }
 }
+
+// 1. 제일 먼저 서비스 전체의 메시지를 구현 : A를 구매하라
+// 2. 다음으로 A의 정보를 구현하라라는 메시지를 구현. 뒤에 작업도 이런 식으로 반복
+// [!] 프로시저와 함수를 구분하자
--- example-test-jc/src/menu/Item/Item.java
+++ example-test-jc/src/menu/Item/Item.java
@@ -1,3 +1,11 @@
+package item;
+
 public abstract class Item {
-    
+    private String name;
+    private int price;
+
+    // [?] return까지 만들어 줬는데 이게 추상 클래스 맞나?
+    public String getData() {
+        return ("상표명 : " + this.name + "가격 : " + Integer.toString(this.price));
+    }
 }
