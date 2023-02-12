7229a3a3ba0b349ecc18cb59c6209d151e03a0cf
Jchan <gkswocks123@gmail.com>
2023-02-12 19:48:26 +0900

    ✨ Item  추상 클래스 생성

---
+++ test-vending-machine/src/main/item/Item.java
@@ -0,0 +1,11 @@
+package item;
+
+public abstract class Item {
+    private String type;
+    private String name;
+    private Number price;
+    
+    public String getType();
+    public String name();
+    public Number price();
+}
