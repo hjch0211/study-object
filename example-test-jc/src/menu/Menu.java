5fd56238e922ee2bc5dc37414da55f4f48f04eb5
Jchan <gkswocks123@gmail.com>
2023-02-12 18:31:54 +0900

    🔥 다시

--- example-test-jc/src/menu/Menu.java
+++
@@ -1,9 +0,0 @@
-package menu;
-import item.Item;
-
-public class Menu {
-    public void showItem(Item item) {
-        String itemData = item.getData(); // [?] 적절하게 캡슐화되었나?
-        System.out.println(itemData);
-    }
-}
